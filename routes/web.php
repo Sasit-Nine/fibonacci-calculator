0def4c569265e65e6088a8205938574029b00920
6710110400 SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2025-03-24 14:46:33 +0700

    Google Auth

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,30 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+Route::get('/auth/google', function () {
+    return Socialite::driver('google')->redirect();
 });
+
+Route::get('/auth/google/callback', function () {
+    $googleUser = Socialite::driver('google')->stateless()->user();
+        $user = User::where('email', $googleUser->email)->first();
+        if(!$user)
+        {
+            $user = User::create(['name' => $googleUser->name, 'email' => $googleUser->email, 'password' => \Hash::make(rand(100000,999999))]);
+        }
+    Auth::login($user);
+    return redirect('/');
+});
+
+Route::middleware(['auth', 'verified'])->get('/', function () {
+        return Inertia::render('dashboard');
+})->name('home');;
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
